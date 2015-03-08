c2369cf6bcd56397f21789684a6c600803e023f2
bullimog <gmb@bullimore.com>
2015-03-08 08:26:24 +0000

    Added more fields

--- app/forms/UrlForms.scala
+++ app/forms/UrlForms.scala
@@ -3,15 +3,44 @@ package forms
 import models.UrlModel
 import play.api.data.Form
 import play.api.data.Forms._
+import play.api.data.format.Formatter
+import play.api.data.FormError
+import play.api.i18n.Messages
 
 object UrlForms {
 
+//create a play Form, to map an html form to a case class (a model)
+// default mapping types can be any of:
+// boolean, checked, date, email, ignored, list, longNumber, nonEmptyText,
+// number, optional, seq, single, sqlDate, or text
 
-  val urlForm = Form(mapping(
-    "url" -> nonEmptyText)(UrlModel.apply)(UrlModel.unapply)
+  val urlForm = Form(
+    mapping(
+      "url" -> nonEmptyText,
+      "protocol" -> text,
+      "overrideProtocol" -> of(validateCheckboxAgainstText("protocol","error.protocol.not.populated"))
+    )(UrlModel.apply)(UrlModel.unapply)
   )
 
+
+
+  //This method is quite generic, so could be moved into a separate validation object
+  def validateCheckboxAgainstText(inputBox:String, error:String) = new Formatter[Boolean] {
+
+    override def bind(key: String, data: Map[String, String]): Either[Seq[FormError], Boolean]= {
+        val checkBox = data.getOrElse(key, "")
+        val inputBoxValue = data.getOrElse(inputBox, "")
+
+        //returning an Either[Seq[FormError], Boolean] (by convention: left=bad, right=good)
+        checkBox match {
+          case "true" if inputBoxValue == "" => Left(List(FormError(key, Messages(error))))
+          case "" => Right(false)              // if its empty, assume its false
+          case _  => Right(checkBox.toBoolean) // otherwise bind boolean value of the checkbox
+        }
+    }
+
+    override def unbind(key: String, value: Boolean): Map[String, String] = {
+      Map(key -> value.toString)
+    }
+  }
 }
-
-
-//.verifying("Needs to be more than two chars", _.length > 2)
--- app/models/UrlModels.scala
+++ app/models/UrlModels.scala
@@ -4,7 +4,7 @@ import org.joda.time.DateTime
 import play.api.libs.json.Json
 
 
-case class UrlModel(url:String)
+case class UrlModel(url:String, protocol:String, overrideProtocol:Boolean)
 
 object UrlModel {
   implicit val formats=Json.format[UrlModel]
