5ae59acd2c093c0d5413dd1bc4f4d71903838b16
bullimog <gmb@bullimore.com>
2016-10-07 19:18:40 +0100

    swapped a case-match for an if

--- app/controllers/AuthAction.scala
+++ app/controllers/AuthAction.scala
@@ -3,6 +3,9 @@ package controllers
 import play.api.mvc._
 import scala.concurrent.Future
 
+
+
+
 object AuthAction extends AuthAction{
   def apply(action: AsyncPlayRequest): Action[AnyContent] = authenticate(action)
 }
@@ -11,9 +14,11 @@ trait AuthAction extends Controller{
   type AsyncPlayRequest = (Request[AnyContent] => Future[Result])
 
   def authenticate(action:AsyncPlayRequest):Action[AnyContent] = Action.async{
-    request => request.session.get("logged-in-key") match {
-      case Some(value) => action(request)
-      case None => Future.successful(Ok("Not Logged in!"))
+    request => {
+      if (request.session.get("logged-in-key").isDefined)
+        action(request)
+      else
+        Future.successful(Forbidden("Not Logged In, man!"))
     }
   }
 }
